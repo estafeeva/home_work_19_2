0934c9188a976cd0a1528180269b5be117da662f
estafeeva <stafeeva.ea@mail.ru>
2024-05-14 17:51:09 +0300

    Add html files and POST method

--- catalog/views.py
+++ catalog/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        # в переменной request хранится информация о методе, который отправлял пользователь
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        # а также передается информация, которую заполнил пользователь
+        print(name, phone, message)
+    return render(request, 'contacts.html')
